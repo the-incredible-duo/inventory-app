ced341205e0b7a955185f69e477244555e051871
DevSalma <devsalma6@gmail.com>
2023-05-22 15:53:49 +0100

    Commented out all server and puclic code for sauce, to make it easier to work with

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,12 @@
-const {Sequelize} = require('sequelize')
-const {sequelize} = require('../db')
+// const {Sequelize} = require('sequelize')
+// const {sequelize} = require('../db')
 
-const Sauce = sequelize.define("sauces", {
-  name: Sequelize.STRING,
-  image: Sequelize.STRING,
-});
+// const Sauce = sequelize.define("sauces", {
+//   name: Sequelize.STRING,
+//   image: Sequelize.STRING,
+// });
 
-module.exports = {
-  db: sequelize,
-  Sauce,
-};
+// module.exports = {
+//   db: sequelize,
+//   Sauce,
+// };
