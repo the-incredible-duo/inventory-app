fb87332866c589dd215c42fdd54e3a6f6653868f
t4nz456 <t4nz1234@gmail.com>
2023-05-23 11:12:41 +0100

    part1

--- public/react/components/Sauce.js
+++ public/react/components/Sauce.js
@@ -1,6 +1,35 @@
 import React from 'react';
+import {  useState, useEffect } from "react";
 
 export const Sauce = (props) => {
+  const[name, setName] = useState(props.sauce.name);
+  const[image, setImage] = useState(props.sauce.event);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  }
+
+  const handleImageChange = (event) => {
+    setImage(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`${apiURL}/sauces/${props.sauce.id}`, {
+      method: `PUT`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({name, image}),
+    })
+    .then((response) => response.json())
+    .then((updatedSauce) => {
+      console.log("Item updated", updatedSauce);
+    })
+    .catch((error) => {
+      console.log("Error updating item: ", error);
+    });
+  }
 
   return <>
     <h3>{props.sauce.name}</h3>
@@ -8,3 +37,4 @@ export const Sauce = (props) => {
   </>
 } 
 	
+// Hello
