8dba6b0754de0195632547cabf657ad3b18d3f8b
DevSalma <devsalma6@gmail.com>
2023-05-23 13:07:32 +0100

    Added test plan for get router and endpoint

---
+++ server/routes/items.js
@@ -0,0 +1,22 @@
+/**
+ * Testing get router and endpoint
+ * 
+ * Import files needed
+ * -require jestglobals
+ * -require supertest(mpm install supertest)
+ * -require item model
+ * -require app
+ * Testing for getting all items
+ * -describe('get all items', () => {
+ * test('get all items', async() => {
+ *  const response = await request(app).get('/endpoint')
+ *  expect(response.body).toBe()
+ * })
+ * })
+ * -describe('get single item')({
+ * test('get one item', async() => {
+ *  const response = await request(app).get('/endpoint')
+ *  expect(respone.body).toBe()
+ *  })
+ * })
+ */
