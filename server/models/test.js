2aae12553a616352cb9260748116da698b6404f5
victoryach <achubsv@gmail.com>
2023-05-23 13:32:22 +0100

    test assertation

---
+++ server/models/test.js
@@ -0,0 +1,63 @@
+Describe("Add Item Front-end Form", () => {
+  it("should render the add item form", () => {
+    // Mock any necessary dependencies or setup your test environment
+    // Render the component or navigate to the page where the form is displayed
+    // Assert that the add item form is rendered correctly
+  });
+
+  it("should submit the form and call the appropriate function", () => {
+    // Mock any necessary dependencies or setup your test environment
+    // Render the component or navigate to the page where the form is displayed
+    // Simulate user input and submit the form
+    // Assert that the appropriate function for adding the item is called with the correct data
+  });
+});
+
+Describe("Express Route to Add the Item", () => {
+  it("should handle the add item request", () => {
+    // Mock any necessary dependencies or setup your test environment
+    // Send a mock request to the add item route
+    // Assert that the request is handled correctly and the item is added to the database
+  });
+});
+
+Describe("Fetch request to add item when form is submitted", () => {
+  it("should send a fetch request to the add item endpoint", () => {
+    // Mock any necessary dependencies or setup your test environment
+    // Simulate user input and submit the form
+    // Assert that a fetch request is sent to the correct endpoint with the correct data
+  });
+});
+
+//to delete item
+
+Describe("Delete Button on Single Item View", () => {
+  it("should render the delete button on the single item view", () => {
+    // Mock any necessary dependencies or setup your test environment
+    // Render the component or navigate to the page where the single item view is displayed
+    // Assert that the delete button is rendered correctly
+  });
+
+  it("should call the appropriate function when the delete button is clicked", () => {
+    // Mock any necessary dependencies or setup your test environment
+    // Render the component or navigate to the page where the single item view is displayed
+    // Simulate clicking the delete button
+    // Assert that the appropriate function for deleting the item is called
+  });
+});
+
+Describe("Express Route to Delete the Item", () => {
+  it("should handle the delete item request", () => {
+    // Mock any necessary dependencies or setup your test environment
+    // Send a mock request to the delete item route
+    // Assert that the request is handled correctly and the item is deleted from the database
+  });
+});
+
+Describe("Fetch request to delete item when button is clicked", () => {
+  it("should send a fetch request to the delete item endpoint", () => {
+    // Mock any necessary dependencies or setup your test environment
+    // Simulate clicking the delete button
+    // Assert that a fetch request is sent to the correct endpoint to delete the item
+  });
+});
