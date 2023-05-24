708600657e4d5a102a6416a38e428ed592f5a5c6
DevSalma <devsalma6@gmail.com>
2023-05-24 15:23:48 +0100

    Added card component and imported Card from react library

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
 
 export const Item = (props) => {
+    const { item } = props;
     return <>
-        <h1>{props.item.title}</h1>
+        <h1>{item.title}</h1>
+        <Card style={{width: '18rems'}}/>
+        <Card.Img variant='top' src='holder.js/100px180'/>
+        <Card.Body>
+            <Card.Title>{item.title}</Card.Title>
+            <Card.Text>{item.description}</Card.Text>
+        </Card.Body>
+        <h1>Hello!</h1>
     </>
 }
 	
