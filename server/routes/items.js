21be23deaa3b9983a909b13d88ffb1631cbdf650
DevSalma <devsalma6@gmail.com>
2023-05-24 10:57:47 +0100

    Added get router for getting 1 item

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,13 +1,22 @@
 const { Router } = require('express');
 const { Item } = require('../models/index.js');
 
-Router.get('/items', async(request, response, next) => {
+Router.get('/items', async(req, res, next) => {
     try{
         const items = await Item.findAll();
-        response.send(items);
+        res.send(items);
     }catch(error){
         next(error);
     }
 })
 
+Router.get('/items/:id', async(req, res, next) => {
+    try{
+        const { id } = req.params;
+        const item = await Item.findByPk(id);
+        res.json(item);
+    }catch(error){
+        next(error);
+    }
+})
 module.exports = Router ;
