794b8efb6d6a3e77c436d760baf313d884bb0557
DevSalma <devsalma6@gmail.com>
2023-05-24 12:01:58 +0100

    Added ItemList.js to map through Item

--- public/react/components/ItemList.js
+++ public/react/components/ItemList.js
@@ -1,2 +1,12 @@
 import React from "react";
 import { Item } from './Item';
+
+export const ItemsList = ({items}) => {
+    return <>
+        {
+            items.map((sauce, index) => {
+                return <Item item={item} key={index}/>
+            })
+        }
+    </>
+}
