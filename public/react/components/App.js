222a189f70d3eb98d26d006aa970ffed85df751d
t4nz456 <t4nz1234@gmail.com>
2023-05-25 16:25:14 +0100

    ItemsList in imported correctly. Removed unnecessary router import. The useState component is updated to use item.title instead of props.items.title. Fixed handleSubmit handler function.

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,28 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { ItemsList } from './ItemList';
 import apiURL from '../api.js';
-import Card from 'react-bootstrap/Card';
 
 export const App = () => {
-    const[items, setItems] = useState([]);
-    async function fetchItem(){
-        try{
-            const response = await fetch(`${apiURL}/items`);
-            const itemsData = await response.json();
-            console.log(itemsData);
-            setItems(itemsData);
-        }catch(err){
-            console.log('Oh an error!',err)
-        }
-    }
-    useEffect(() => {
-        fetchItem();
-    },[]);
-    
-        return(
-            <main>
+  const [items, setItems] = useState([]);
 
-            <ItemsList items={items}/>
-            </main>
-        )
+  async function fetchItems() {
+    try {
+      const response = await fetch(`${apiURL}/items`);
+      const itemsData = await response.json();
+      console.log(itemsData);
+      setItems(itemsData);
+    } catch (err) {
+      console.log('Oh, an error!', err);
+    }
+  }
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  return (
+    <main>
+      <ItemsList items={items} />
+    </main>
+  );
 }
--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -1,64 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from 'react-bootstrap/Card';
-import {  useState, useEffect } from "react";
-import { router } from "./server/routes/sauces.js";
-
+import apiURL from '../api.js';
 
 export const Item = (props) => {
-    const { item } = props;
-    const[title, setTitle] = useState(props.items.title);
-    const[image, setImage] = useState(props.items.event);
+  const { item } = props;
+  const [title, setTitle] = useState(item.title);
+  const [image, setImage] = useState(item.image);
 
-    const handleTitleChange = (event) => {
-        setTitle(event.target.value);    // Calls the current event handler
-      }
-    
-      const handleImageChange = (event) => {
-        setImage(event.target.value);
-      }
-    
-      const handleSubmit = (event) => {   // Fetch request to update item when form is submitted
-        event.preventDefault();
-        fetch(`${apiURL}/items/${props.items.id}`, {   // Make fetch request to update the item
-          method: `PUT`,
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({title, image}),  // Converts values to JSON strings
-        })
-        .then((response) => response.json())
-        .then((updatedItem) => {   // Handle the updated sauce data
-          console.log("Item updated", updatedItem);
-        })
-        .catch((error) => {   // Handle the error if needed
-          console.log("Error updating item: ", error);
-        });
-      }
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  }
 
-    return (    // Single item form view
-        <>
-            <h1>{item.title}</h1>
-            <Card style={{width: '18rems'}}/>
-            <Card.Img variant='top' src='holder.js/100px180'/>
-            <Card.Body>
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text>{item.description}</Card.Text>
-            </Card.Body>
+  const handleImageChange = (event) => {
+    setImage(event.target.value);
+  }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`${apiURL}/items/${item.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, image }),
+    })
+      .then((response) => response.json())
+      .then((updatedItem) => {
+        console.log("Item updated", updatedItem);
+      })
+      .catch((error) => {
+        console.log("Error updating item:", error);
+      });
+  }
 
-            <h3>{props.items.name}</h3>   
-            <form onSubmit={handleSubmit}>
-                <label>
-                Name:
-                <input type="text" value={title} onChange={handleTitleChange} />
-                </label>
-                <label>
-                Image URL:
-                <input type="text" value={image} onChange={handleImageChange} />
-                </label>
-                <button onClick={handleSubmit} type="submit">Update</button>
-            </form>
-            </>
-    );
+  return (
+    <>
+      <h1>{item.title}</h1>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={item.image} alt={item.title} />
+        <Card.Body>
+          <Card.Title>{item.title}</Card.Title>
+          <Card.Text>{item.description}</Card.Text>
+        </Card.Body>
+      </Card>
+
+      <h3>{item.name}</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={title} onChange={handleTitleChange} />
+        </label>
+        <label>
+          Image URL:
+          <input type="text" value={image} onChange={handleImageChange} />
+        </label>
+        <button type="submit">Update</button>
+      </form>
+    </>
+  );
 }
-	
