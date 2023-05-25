1e688c25446bbbde8ac40ecc8391d4f649453e8e
DevSalma <devsalma6@gmail.com>
2023-05-25 11:18:11 +0100

    Accepted changes

---
+++ server/models/model.test.js
@@ -0,0 +1,25 @@
+//Testing Models
+/**
+ * Define item variable in global scope
+ * Make sure to wipe db before new tests
+ * Clear db after tests
+ * Test for:
+ * -name
+ * -description
+ * -prices
+ * -category
+ * -image
+ * Make sure to wipe db and create new item before new tests
+ * beforeAll(async () => {
+ *      await db.sync({force: true})
+ *      item = await Item.create({
+ *          name: ''
+ *          description: ''
+ *          prices: ''
+ *          category: ''
+ *          image: ''
+ *  })
+ * })
+ * Clear db after tests
+ * -- afterAll(async () => await db.sync({force: true}))
+ */
