7e92fce582de1cf6059b9f745a01c5dafd8c8952
DevSalma <devsalma6@gmail.com>
2023-05-26 10:24:39 +0100

    Destructured sequelize import in index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = require("../db.js");
+const { Sequelize, DataTypes } = require('sequelize');
+const {sequelize} = require('../db.js');
 
 const Item = sequelize.define("item", {
   title: {
