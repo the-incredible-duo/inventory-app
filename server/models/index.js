a4112a7f4181834287de56d73b7232b9d6119c0d
DevSalma <devsalma6@gmail.com>
2023-05-26 09:57:21 +0100

    Refactored the sequelize model to make it easier to work with

--- server/models/index.js
+++ server/models/index.js
@@ -2,11 +2,21 @@ const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../db.js');
 
 const Item = sequelize.define('item',{
-    title: DataTypes.STRING,
-    price: DataTypes.FLOAT,
-    description: DataTypes.STRING,
-    category: DataTypes.STRING,
-    image: DataTypes.STRING
+    title: {
+        type: DataTypes.STRING,
+    },
+    price: {
+        type: DataTypes.FLOAT,
+    },
+    description: {
+        type: DataTypes.STRING,
+    },
+    category: {
+        type: DataTypes.STRING,
+    },
+    image: {
+        type: DataTypes.STRING
+    }
 })
 
 module.exports = {
